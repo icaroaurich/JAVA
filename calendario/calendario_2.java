6fbd3b987119450638c2e4824c46dface494c837
SK <79989219+icaroaurich@users.noreply.github.com>
2022-08-09 15:47:26 -0300

    Add files via upload

---
+++ calendario/calendario_2.java
@@ -0,0 +1,47 @@
+// calendário
+/*
+ * Planejamento
+ * 31..JANEIRO
+ * C...FEVEREIRO
+ * 31..MARÇO
+ * 30..ABRIL
+ * 31..MAIO
+ * 30..JUNHO
+ * 31..JULHO
+ * 31..AGOSTO
+ * 30..SETEMBRO
+ * 31..OUTRUBRO
+ * 30..NOVEMBRO
+ * 31..DESEMBRO 
+ * 
+ */
+import java.util.Scanner;
+public class Main{
+    public static void main(String agrs[]){
+		Scanner input = new Scanner(System.in);
+		//System.out.print("Digite o ano: ");
+		
+		int ano = 2022;//input.nextInt();
+		int dia = 1;
+		int mes = 1;
+		int i;
+		
+		while (mes < 2){
+		    System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------");
+		    
+    		for (dia=1;dia<31;dia++){
+    		    if (dia < 10){
+    		        String sdia = "0" + String.valueOf(dia);
+        		    System.out.print(sdia+"/"+mes+"/"+ano+"  |  ");
+              }else{
+                  System.out.print(dia+"/"+mes+"/"+ano+"  |  ");
+        		    if (dia == 10){
+        		        System.out.println("");
+            		    }else if (dia == 20){
+        		        System.out.println("");
+		        }}}
+		    System.out.println("");
+		    System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------");
+		    mes++;
+		    }}
+    }
